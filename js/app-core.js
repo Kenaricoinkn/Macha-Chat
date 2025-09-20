1c92016ed4af87151b38e206595020cf09c50b14
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-21 04:25:16 +0700

    Update app-core.js

--- js/app-core.js
+++ js/app-core.js
@@ -1,15 +1,5 @@
-import { setupRoutes, routeTo } from './router.js'
-import { bindAuthForms, watchAuth } from './auth.js'
-import { bindComposer, startFeeds } from './posts.js'
+import './firebase-init.js'
+import { watchRedirectAfterLogin } from './auth.js'
 
-setupRoutes()
-bindAuthForms()
-bindComposer()
-
-// Watch auth: kalau login tampilkan feed
-watchAuth((user)=>{
-  if(user){
-    startFeeds()
-    routeTo('home')
-  }
-})
+// cukup import auth.js; ia sudah bind semua handler UI
+watchRedirectAfterLogin()
