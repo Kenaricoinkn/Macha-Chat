60b3a1f83a916afebaa8c68968be2e5f616184d9
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-21 04:48:24 +0700

    Update app-core.js

--- js/app-core.js
+++ js/app-core.js
@@ -1,5 +1,2 @@
-import './firebase-init.js'
-import { watchRedirectAfterLogin } from './auth.js'
-
-// cukup import auth.js; ia sudah bind semua handler UI
-watchRedirectAfterLogin()
+import './auth.js'
+// cukup import auth.js; ia sudah bind semua handler
