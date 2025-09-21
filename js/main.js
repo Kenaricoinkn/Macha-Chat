7c01c06f6f16cc9f4beb7e639c0fda62b0d02cd1
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-21 23:28:26 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // js/main.js
 import { onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js'
-import { auth } from './firebase-init.js'
+import { auth } from './config.js'
 import { initComposer, startFeed } from './composer_feed.js'
 
 const $ = (s)=>document.querySelector(s)
