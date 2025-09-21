01c8cce3450e45d8050a48f651549bfd64d60eff
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-21 20:08:38 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { app, auth, $, toast } from './config.js'
-import { onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js'
+import { auth, $, toast } from './config.js'
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js'
 import { initMenuUI } from './menu.js'
 import { initComposer, startFeed, renderStories } from './composer_feed.js'
 
@@ -8,13 +8,11 @@ function ready(fn){ document.readyState==='loading' ? document.addEventListener(
 ready(()=>{
   onAuthStateChanged(auth, (user)=>{
     if(!user){ location.replace('./index.html'); return }
-    // tombol + & search (dummy action)
     $('#btnPlus')?.addEventListener('click', ()=>toast('Aksi cepat coming soon'))
     $('#btnSearch')?.addEventListener('click', ()=>toast('Pencarian coming soon'))
-
     initMenuUI(user)
     initComposer(user)
-    renderStories(user.displayName || user.email)
+    renderStories(user.displayName||user.email)
     startFeed(user)
   })
 })
