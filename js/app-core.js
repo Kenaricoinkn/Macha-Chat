8a010bbdefb8d35672c543efc4218c44ec71836f
Kenaricoin <kenaricoinkn@gmail.com>
2025-10-05 23:04:52 +0700

    Delete js directory

--- js/app-core.js
+++
@@ -1,9 +0,0 @@
-// js/app-core.js
-import { initAuthUI } from './auth.js';
-
-// pastikan jalan setelah DOM siap
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', initAuthUI);
-} else {
-  initAuthUI();
-}
