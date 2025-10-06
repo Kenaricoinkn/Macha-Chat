f2b2bfed4de4df9d732cc41aeaddab3b2bee2742
Kenaricoin <kenaricoinkn@gmail.com>
2025-10-06 07:03:14 +0700

    Update menu-popup.js

--- js/menu-popup.js
+++ js/menu-popup.js
@@ -1,7 +1,21 @@
 // ============================
 // MENU POPUP WITH SETTINGS + INTERACTIVE (v4)
 // ============================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDIsTmg0_-rcz9tH3U-_sZuWk7sUOLgMSw",
+  authDomain: "macha-chat.firebaseapp.com",
+  projectId: "macha-chat",
+  storageBucket: "macha-chat.firebasestorage.app",
+  messagingSenderId: "576142384286",
+  appId: "1:576142384286:web:cfed000ac8a50b662a6f8f",
+  measurementId: "G-6ZMFHMZVEK"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const menuBtn = document.getElementById("menuBtn");
 const menuPopup = document.getElementById("menuPopup");
 
