a838883f1dca2f7b51a859719682ed70803bb46e
Kenaricoin <kenaricoinkn@gmail.com>
2025-10-06 08:24:30 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// ✅ Firebase CONFIG GLOBAL (digunakan bersama)
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIsTmg0_-rcz9tH3U-_sZuWk7sUOLgMSw",
+  authDomain: "macha-chat.firebaseapp.com",
+  projectId: "macha-chat",
+  storageBucket: "macha-chat.firebasestorage.app",
+  messagingSenderId: "576142384286",
+  appId: "1:576142384286:web:cfed000ac8a50b662a6f8f",
+  measurementId: "G-6ZMFHMZVEK"
+};
+
+// 🧠 Gunakan app yang sudah ada, hindari duplikasi init
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
